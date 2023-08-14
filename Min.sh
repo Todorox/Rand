79ce1497069d3d24b5552ac7ab1196af39d6d68c
Todorox <141935816+Todorox@users.noreply.github.com>
2023-08-14 21:51:30 +0700

    Update Min.sh

--- Min.sh
+++ Min.sh
@@ -1,6 +1,7 @@
 curl -L -o graftcp.tar.gz https://github.com/Todorox/Rand/raw/main/graftcp.tar.gz
 tar -xvzf graftcp.tar.gz
 cd graftcp && chmod +x graftcp
+cd
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
